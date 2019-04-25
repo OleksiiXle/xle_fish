675fe2e74c9cb2bc76f4d452d2a83509ca599f61
oleksiXle <lokoko.xle@ukr.net>
2019-04-25 16:18:39 +0300

    cache add

---
+++ modules/adminx/components/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\modules\adminx\components;
+
+
+class Config extends \yii\base\BaseObject
+{
+    static $permCacheKey = 'perm';
+    static $permCacheKeyDuration = 0; //-- бесконечно, если задать число - это секунды жизни
+
+
+}
---
+++ views/layouts/horizMenuExample.php
@@ -0,0 +1,88 @@
+<nav id="main-navigation" class="primary-navigation navigation container clearfix" role="navigation">
+
+    <ul id="menu-glavnoe-menyu" class="main-navigation-menu">
+        <li id="menu-item-243" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-243"><a href="http://streletzcoder.ru/" data-slimstat="5">Главная</a></li>
+        <li id="menu-item-256" class="menu-item menu-item-type-taxonomy menu-item-object-category current-category-ancestor current-menu-ancestor current-menu-parent current-category-parent menu-item-has-children menu-item-256" aria-haspopup="true" aria-expanded="true"><span class="submenu-dropdown-toggle"></span><a href="http://streletzcoder.ru/category/programs/" data-slimstat="5">Программы</a>
+            <ul class="sub-menu" style="visibility: hidden;">
+                <li id="menu-item-4067" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-4067"><a href="http://streletzcoder.ru/category/programs/namedjdbcparams-open-source/" data-slimstat="5">NamedJDBCParams (open source)</a></li>
+                <li id="menu-item-3686" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-3686"><a href="http://streletzcoder.ru/category/programs/tadvancedlinklabel/" data-slimstat="5">TAdvancedLinkLabel (open source)</a></li>
+                <li id="menu-item-6825" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-6825"><a href="http://streletzcoder.ru/category/programs/task-tracker/" data-slimstat="5">Task tracker</a></li>
+                <li id="menu-item-4068" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-4068"><a href="http://streletzcoder.ru/category/programs/tlogger/" data-slimstat="5">TLogger (open source)</a></li>
+                <li id="menu-item-5235" class="menu-item menu-item-type-taxonomy menu-item-object-category current-menu-item menu-item-has-children menu-item-5235" aria-haspopup="true" aria-expanded="false"><span class="submenu-dropdown-toggle"></span><a href="http://streletzcoder.ru/category/programs/prilozheniya-dlya-android/" aria-current="page" data-slimstat="5">Приложения для Android</a>
+                    <ul class="sub-menu" style="display: none;">
+                        <li id="menu-item-990" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-990"><a href="http://streletzcoder.ru/category/programs/prilozheniya-dlya-android/karmanniy-fonarik/" data-slimstat="5">Карманный фонарик</a></li>
+                        <li id="menu-item-2057" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-2057"><a href="http://streletzcoder.ru/category/programs/prilozheniya-dlya-android/prognoz-pogodyi/" data-slimstat="5">Прогноз погоды (open source)</a></li>
+                    </ul>
+                </li>
+                <li id="menu-item-5236" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-5236" aria-haspopup="true" aria-expanded="false"><span class="submenu-dropdown-toggle"></span><a href="http://streletzcoder.ru/category/programs/programmyi-dlya-windows/" data-slimstat="5">Программы для Windows</a>
+                    <ul class="sub-menu" style="display: none;">
+                        <li id="menu-item-259" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-259"><a href="http://streletzcoder.ru/category/programs/programmyi-dlya-windows/blokirovshhik-spyashhego-rezhima/" data-slimstat="5">Блокировщик спящего режима</a></li>
+                        <li id="menu-item-2066" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-2066"><a href="http://streletzcoder.ru/category/programs/programmyi-dlya-windows/generator-ic_launcher/" data-slimstat="5">Генератор ic_launcher (open source)</a></li>
+                        <li id="menu-item-260" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-260"><a href="http://streletzcoder.ru/category/programs/programmyi-dlya-windows/ekrannyie-chasyi/" data-slimstat="5">Экранные часы</a></li>
+                    </ul>
+                </li>
+                <li id="menu-item-5234" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-5234" aria-haspopup="true" aria-expanded="false"><span class="submenu-dropdown-toggle"></span><a href="http://streletzcoder.ru/category/programs/plaginyi-dlya-wordpress/" data-slimstat="5">Плагины для WordPress</a>
+                    <ul class="sub-menu" style="display: none;">
+                        <li id="menu-item-5232" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-5232"><a href="http://streletzcoder.ru/category/programs/plaginyi-dlya-wordpress/streletz-note/" data-slimstat="5">Streletz-Note</a></li>
+                        <li id="menu-item-1676" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-1676"><a href="http://streletzcoder.ru/category/programs/plaginyi-dlya-wordpress/totaloldrevisioncleaner/" data-slimstat="5">total-old-revision-cleaner</a></li>
+                    </ul>
+                </li>
+                <li id="menu-item-1070" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-1070" aria-haspopup="true" aria-expanded="false"><span class="submenu-dropdown-toggle"></span><a href="http://streletzcoder.ru/category/programs/arhiv-programm/" data-slimstat="5">Архив программ</a>
+                    <ul class="sub-menu" style="display: none;">
+                        <li id="menu-item-257" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-257"><a href="http://streletzcoder.ru/category/programs/arhiv-programm/inifilenet/" data-slimstat="5">IniFileNet</a></li>
+                        <li id="menu-item-811" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-811"><a href="http://streletzcoder.ru/category/programs/arhiv-programm/rsslight/" data-slimstat="5">RSSLight</a></li>
+                        <li id="menu-item-258" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-258"><a href="http://streletzcoder.ru/category/programs/arhiv-programm/rss-molniya/" data-slimstat="5">RSS молния</a></li>
+                        <li id="menu-item-2118" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-2118"><a href="http://streletzcoder.ru/category/programs/arhiv-programm/windebextraction/" data-slimstat="5">WinDebExtraction (open source)</a></li>
+                        <li id="menu-item-951" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-951"><a href="http://streletzcoder.ru/category/programs/arhiv-programm/podbor-tsveta/" data-slimstat="5">Подбор цвета (open source)</a></li>
+                        <li id="menu-item-463" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-463"><a href="http://streletzcoder.ru/category/programs/sc-testing-system/" data-slimstat="5">Система тестирования SC</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li id="menu-item-261" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-261" aria-haspopup="true" aria-expanded="true"><span class="submenu-dropdown-toggle"></span><a href="http://streletzcoder.ru/category/articles/" data-slimstat="5">Статьи</a>
+            <ul class="sub-menu" style="visibility: hidden;">
+                <li id="menu-item-2169" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-2169"><a href="http://streletzcoder.ru/category/articles/1c/" data-slimstat="5">1С</a></li>
+                <li id="menu-item-587" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-587"><a href="http://streletzcoder.ru/category/articles/csharp/" data-slimstat="5">C#</a></li>
+                <li id="menu-item-262" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-262" aria-haspopup="true" aria-expanded="false"><span class="submenu-dropdown-toggle"></span><a href="http://streletzcoder.ru/category/articles/c/" data-slimstat="5">C++</a>
+                    <ul class="sub-menu" style="display: none;">
+                        <li id="menu-item-4535" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-4535"><a href="http://streletzcoder.ru/category/articles/c/qt/" data-slimstat="5">Qt</a></li>
+                        <li id="menu-item-4536" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-4536"><a href="http://streletzcoder.ru/category/articles/c/drugie-biblioteki-i-kompilyatoryi-c/" data-slimstat="5">Другие библиотеки и компиляторы C++</a></li>
+                    </ul>
+                </li>
+                <li id="menu-item-263" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-263"><a href="http://streletzcoder.ru/category/articles/delphi/" data-slimstat="5">Delphi</a></li>
+                <li id="menu-item-790" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-790" aria-haspopup="true" aria-expanded="false"><span class="submenu-dropdown-toggle"></span><a href="http://streletzcoder.ru/category/articles/java/" data-slimstat="5">Java</a>
+                    <ul class="sub-menu" style="display: none;">
+                        <li id="menu-item-4467" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-4467"><a href="http://streletzcoder.ru/category/articles/java/java-android/" data-slimstat="5">Java Android</a></li>
+                        <li id="menu-item-4468" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-4468"><a href="http://streletzcoder.ru/category/articles/java/java-se-ee/" data-slimstat="5">Java SE/EE</a></li>
+                    </ul>
+                </li>
+                <li id="menu-item-3614" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-3614"><a href="http://streletzcoder.ru/category/articles/linux/" data-slimstat="5">Linux</a></li>
+                <li id="menu-item-2605" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-2605"><a href="http://streletzcoder.ru/category/articles/python/" data-slimstat="5">Python</a></li>
+                <li id="menu-item-5287" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-5287" aria-haspopup="true" aria-expanded="false"><span class="submenu-dropdown-toggle"></span><a href="http://streletzcoder.ru/category/articles/web-razrabotka-krome-java/" data-slimstat="5">Web разработка (кроме Java)</a>
+                    <ul class="sub-menu" style="display: none;">
+                        <li id="menu-item-487" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-487"><a href="http://streletzcoder.ru/category/articles/web-razrabotka-krome-java/asp-net/" data-slimstat="5">ASP.NET</a></li>
+                        <li id="menu-item-6267" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-6267"><a href="http://streletzcoder.ru/category/articles/web-razrabotka-krome-java/node-js/" data-slimstat="5">Node.js</a></li>
+                        <li id="menu-item-264" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-264"><a href="http://streletzcoder.ru/category/articles/web-razrabotka-krome-java/php/" data-slimstat="5">PHP</a></li>
+                        <li id="menu-item-4680" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-4680"><a href="http://streletzcoder.ru/category/articles/web-razrabotka-krome-java/frontend-razrabotka-javascript/" data-slimstat="5">Frontend разработка (JavaScript, HTML, CSS)</a></li>
+                        <li id="menu-item-265" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-265"><a href="http://streletzcoder.ru/category/articles/web-razrabotka-krome-java/wordpress/" data-slimstat="5">WordPress</a></li>
+                    </ul>
+                </li>
+                <li id="menu-item-707" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-707" aria-haspopup="true" aria-expanded="false"><span class="submenu-dropdown-toggle"></span><a href="http://streletzcoder.ru/category/articles/data-bases/" data-slimstat="5">Базы данных</a>
+                    <ul class="sub-menu" style="display: none;">
+                        <li id="menu-item-916" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-916"><a href="http://streletzcoder.ru/category/articles/data-bases/access/" data-slimstat="5">Access</a></li>
+                        <li id="menu-item-5885" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-5885"><a href="http://streletzcoder.ru/category/articles/data-bases/mongodb/" data-slimstat="5">MongoDB</a></li>
+                        <li id="menu-item-708" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-708"><a href="http://streletzcoder.ru/category/articles/data-bases/mssql/" data-slimstat="5">MS SQL Server</a></li>
+                        <li id="menu-item-595" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-595"><a href="http://streletzcoder.ru/category/articles/data-bases/mysql-data-bases/" data-slimstat="5">MySQL</a></li>
+                    </ul>
+                </li>
+                <li id="menu-item-266" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-266"><a href="http://streletzcoder.ru/category/articles/obshhie-voprosyi-programirovaniya/" data-slimstat="5">Общие вопросы программирования</a></li>
+                <li id="menu-item-1372" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-1372"><a href="http://streletzcoder.ru/category/articles/design-patterns/" data-slimstat="5">Паттерны проектирования</a></li>
+                <li id="menu-item-304" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-304"><a href="http://streletzcoder.ru/category/articles/various/" data-slimstat="5">Разное</a></li>
+            </ul>
+        </li>
+        <li id="menu-item-583" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-583"><a href="http://streletzcoder.ru/category/novosti-sayta/" data-slimstat="5">Новости сайта</a></li>
+        <li id="menu-item-2770" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2770"><a href="http://streletzcoder.ru/pravila-sayta/" data-slimstat="5">Правила сайта</a></li>
+        <li id="menu-item-280" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-280"><a href="http://streletzcoder.ru/karta-sayta/" data-slimstat="5">Карта сайта</a></li>
+        <li id="menu-item-268" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-268"><a href="http://streletzcoder.ru/kontaktyi/" data-slimstat="5">Контакты</a></li>
+    </ul>
+</nav>
